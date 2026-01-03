9e23a00f6a013bd73ac8f3153440891153460d34
jackofall1232 <jackofall1232@gmail.com>
2026-01-03 12:17:49 -0700

    Create uninstall.php for plugin cleanup
    Add uninstall script for WordPress Database Inspector plugin.

---
+++ uninstall.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Uninstall script for WordPress Database Inspector.
+ *
+ * This plugin does not store persistent data, create custom tables,
+ * or modify WordPress options outside of manual user actions.
+ *
+ * For safety, no database data is deleted on uninstall.
+ *
+ * @package WP_Database_Inspector
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// Intentionally left blank.
+// No persistent data to clean up.
